4fd118af9f18a02d547153e0480b7cdedbdbd955
ahoyland <alex.hoyland@gmail.com>
2015-04-09 23:00:20 -0400

    First commit

---
+++ install.pl
@@ -0,0 +1,6 @@
+#First install Git, and make sure to select the option to enable the CLI
+#Then install perl and node.js and open up the npm command prompt
+#Then run this script
+
+system("npm update");
+system("bower update");
