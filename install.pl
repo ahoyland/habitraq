30c64ab2b4b519be9198e03685dc08178708c4a3
ahoyland <alex.hoyland@gmail.com>
2015-04-15 01:46:04 -0400

    Mocked up front page

--- install.pl
+++ install.pl
@@ -1,6 +1,23 @@
-#First install Git, and make sure to select the option to enable the CLI
-#Then install perl and node.js and open up the npm command prompt
-#Then run this script
+#This script will completely set up the development environment for Habitraq
+#under fedora core.  Copy to the desired directory and run as root.  Repo will 
+#be created in ./habitraq
 
+#Install applications
+#system("yum install git")
+#system("yum install node.js");
+#system("yum install apache");
+#system("yum install mysql");
+
+#Configure apache
+#TODO
+
+#Configure MySQL
+#TODO
+
+#Clone the github repo
+system("git clone http://github.com/ahoyland/habitraq.git habitraq");
+
+#Initialize the development environment
+chdir("./habitraq");
 system("npm update");
 system("bower update");
